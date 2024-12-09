6a81fa43458d6c28b23701a9a25a1d4cf71cb677
CartonLukas <lukas.carton2@student.vives.be>
2024-12-09 14:55:54 +0100

    tesr

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ app.use(express.json())
 
 const port = process.env.PORT || 3000
 
-
+//test
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Connected to MongoDB!'))
     .catch(err => console.error("Error connecting to DB :", err))
